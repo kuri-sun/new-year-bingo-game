99160e64395d69878eb703572c5a56beccc6f3fd
haruki <soccer_haruki15@me.com>
2023-12-27 20:55:13 -0800

    fix: elastic beanstalk environment, backend refuses the frontend's connection

--- frontend/src/utils/Constants.ts
+++ frontend/src/utils/Constants.ts
@@ -1,3 +1,5 @@
-export const REST_API_URL = "http://backend:3000";
-export const WEB_SOCKET_API_URL = "http://backend:4000";
+export const REST_API_URL =
+  "http://bingo-game-env.eba-9snq8cah.us-east-1.elasticbeanstalk.com:3000";
+export const WEB_SOCKET_API_URL =
+  "http://bingo-game-env.eba-9snq8cah.us-east-1.elasticbeanstalk.com:4000";
 export const LS_USER_KEY = "currentUsers";
