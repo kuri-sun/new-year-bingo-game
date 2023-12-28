5b61f8fea7d84e09087a8aa327facb800305cd4a
haruki <soccer_haruki15@me.com>
2023-12-27 20:49:26 -0800

    fix: elastic beanstalk environment, backend refuses the frontend's connection

--- frontend/src/utils/Constants.ts
+++ frontend/src/utils/Constants.ts
@@ -1,3 +1,3 @@
-export const REST_API_URL = "http://localhost:3000";
-export const WEB_SOCKET_API_URL = "http://localhost:4000";
+export const REST_API_URL = "http://backend:3000";
+export const WEB_SOCKET_API_URL = "http://backend:4000";
 export const LS_USER_KEY = "currentUsers";
